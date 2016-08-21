b356679a7bbf53c818a114c73945a0378c939f97
HelenaOliveira1 <helenna54319@gmail.com>
2016-08-21 12:31:30 -0300

    Questão 8
    Questão 8

--- questao-8.py
+++ questao-8.py
@@ -1,14 +1,17 @@
 print("Questão 8")
 print("")
 
-n1 = eval(input("Digite o primeiro número: "))
-n2 = eval(input("Digite o segundo número: "))
-n3 = eval(input("Digite o terceiro número: "))
-n4 = eval(input("Digite o quarto número: "))
-n5 = eval(input("Digite o quinto número: "))
-
-result = n1+ n2+ n3+ n4+ n5
-print("A soma dos números", n1, "+", n2, "+", n3, "+", n4, "+", n5, "é igual a", result)
+quant = int(input("Informe a quantidade de números: "))
 print("")
-media = result/5
-print("A média destes números é igual a: ", media)
+i = 0
+soma = 0
+
+while (i < quant):
+    num = int(input("Informe o número: "))
+    soma += num
+    i += 1
+    continue
+
+media = soma/quant
+print("")
+print("A média dos números é: ", media)
